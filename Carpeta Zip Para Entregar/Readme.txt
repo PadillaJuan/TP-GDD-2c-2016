b34e9a4f285a43bc36b2f3599154bfb8b453f249
unknown <juanpadilla.jip@gmail.com>
2016-10-17 19:24:29 -0300

    Estructura carpetas. Inicio solucion .NET

---
+++ Carpeta Zip Para Entregar/Readme.txt
@@ -0,0 +1,6 @@
+GRUPO 5 DREAM_TEAM
+Integrantes del grupo:
+AMARILLO, Juan Alberto. 		Legajo Nº: 152.508-6 .Curso: K3021
+BIONDI BONOMINI, Marco Fernando.	Legajo Nº: 153.052-5. Curso: K3071 
+DÁTTOLI, Lucas Nahuel. 			Legajo Nº: 152.771-0. Curso: K3022
+PADILLA, Juan Ignacio.(Responsable)	Legajo Nº: 153.064-1. Curso: K3122
---
+++ Carpeta Zip Para Entregar/src/ClinicaFrba/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ClinicaFrba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imagenINicial = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenINicial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Presione aqui para continuar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(299, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Clínica FRBA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(281, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bienvenido al sistema de";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // imagenINicial
+            // 
+            this.imagenINicial.AccessibleDescription = "imagenInicial";
+            this.imagenINicial.AccessibleName = "imagenInicial";
+            this.imagenINicial.Image = global::ClinicaFrba.Properties.Resources.UpuqoFNP;
+            this.imagenINicial.Location = new System.Drawing.Point(224, 90);
+            this.imagenINicial.Name = "imagenINicial";
+            this.imagenINicial.Size = new System.Drawing.Size(335, 328);
+            this.imagenINicial.TabIndex = 0;
+            this.imagenINicial.TabStop = false;
+            this.imagenINicial.Tag = "imagenInicial";
+            this.imagenINicial.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleDescription = "imagenInicial";
+            this.pictureBox1.AccessibleName = "imagenInicial";
+            this.pictureBox1.Location = new System.Drawing.Point(224, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(335, 328);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "imagenInicial";
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(700, 521);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imagenINicial);
+            this.Name = "Form1";
+            this.Text = "Clinica FRBA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagenINicial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagenINicial;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
+
