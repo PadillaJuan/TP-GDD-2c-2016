bdb9ba1a163a11cdaf6f7dc3a1327946c67edc36
Juan Padilla <juanpadilla.jip@gmail.com>
2016-11-10 12:05:52 -0300

    Buscador de afiliado. Falta llenado de tabla que lo hago a la brevedad

--- Carpeta Zip Para Entregar/src/ClinicaFrba/Buscar Afiliado/BuscarAfi.Designer.cs
+++ Carpeta Zip Para Entregar/src/ClinicaFrba/Buscar Afiliado/BuscarAfi.Designer.cs
@@ -34,12 +34,18 @@
             this.gD2C2016DataSet = new ClinicaFrba.GD2C2016DataSet();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gD2C2016DataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gD2C2016DataSet)).BeginInit();
@@ -50,9 +56,9 @@
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.DataSource = this.gD2C2016DataSetBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 93);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 140);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(730, 178);
+            this.dataGridView1.Size = new System.Drawing.Size(730, 298);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -69,7 +75,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Location = new System.Drawing.Point(13, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 13);
             this.label1.TabIndex = 1;
@@ -77,35 +83,29 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(164, 39);
+            this.textBox1.Location = new System.Drawing.Point(164, 25);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(290, 20);
             this.textBox1.TabIndex = 2;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 24;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(16, 386);
+            this.button2.Location = new System.Drawing.Point(16, 444);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 47);
             this.button2.TabIndex = 4;
             this.button2.Text = "Dar de baja";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // button1
             // 
-            this.button3.Location = new System.Drawing.Point(16, 461);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 49);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(16, 497);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 49);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Agregar familiar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
@@ -129,24 +129,93 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(502, 28);
+            this.button6.Location = new System.Drawing.Point(644, 25);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(102, 40);
+            this.button6.Size = new System.Drawing.Size(102, 91);
             this.button6.TabIndex = 23;
             this.button6.Text = "Buscar";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(485, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(146, 17);
+            this.checkBox1.TabIndex = 25;
+            this.checkBox1.Text = "Buscar con Nª de afiliado";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(485, 64);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(118, 17);
+            this.checkBox2.TabIndex = 28;
+            this.checkBox2.Text = "Buscar con nombre";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(290, 20);
+            this.textBox2.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Ingresar el nombre del afiliado";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(485, 99);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(119, 17);
+            this.checkBox3.TabIndex = 31;
+            this.checkBox3.Text = "Buscar con apellido";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(290, 20);
+            this.textBox3.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 13);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Ingresar el apellido del afiliado";
             // 
             // BuscarAfi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -167,11 +236,17 @@
         private GD2C2016DataSet gD2C2016DataSet;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Carpeta Zip Para Entregar/src/ClinicaFrba/Buscar Afiliado/BuscarAfi.cs
+++ Carpeta Zip Para Entregar/src/ClinicaFrba/Buscar Afiliado/BuscarAfi.cs
@@ -25,7 +25,7 @@ namespace ClinicaFrba.BuscarAfiliado
             long id = getId(e);
             if (id == null)
             {
-                MessageBox.Show("No se ha buscado ningun afiliado", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("No se ha seleccionado", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
@@ -67,6 +67,43 @@ namespace ClinicaFrba.BuscarAfiliado
         {
 
         }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            String query = generateQuery();
+            SqlConnection conn = (new BDConnection()).getMiConnectionSQL();
+            SqlCommand cm = new SqlCommand(query,conn);
+
+        }
+
+        public String generateQuery()
+        {
+            bool flag= false;
+            string query = "SELECT * FROM afiliados WHERE ";
+            if (checkBox1.Checked) { 
+                String id,rel;
+                id = String.Format("{0}",int.Parse(textBox1.Text)/100);
+                rel = String.Format("{0}", int.Parse(textBox1.Text) % 100);
+                query += String.Format("af_id = {0} AND af_rel_id = {1} ", id, rel);
+                flag = true;
+            }
+            if (checkBox2.Checked)
+            {
+                if (flag) { query += "AND";};
+                query += String.Format("af_nombre like {0}",textBox2.Text);
+                flag = true;
+            }
+            if (checkBox3.Checked)
+            {
+                if (flag) { query += "AND"; };
+                query += String.Format("af_apellido like {0}",textBox3.Text);
+            }
+            return query;
+
+        }
+
+ 
+
 
 
 
