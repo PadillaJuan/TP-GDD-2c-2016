21e570200c8f4c497f3cfa2b96164ca9cc3bc941
Juan Padilla <juanpadilla.jip@gmail.com>
2016-11-11 16:06:40 -0300

    Cambios SQL

--- Carpeta Zip Para Entregar/data/Procedures.sql
+++ Carpeta Zip Para Entregar/data/Procedures.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE bajaAfiliado
 	SET af_status = 'D'
 	SET af_fechaBaja = GETDATE()
 	WHERE af_id = @id AND af_rel_id = @id_rel
-
+END
 
 CREATE PROCEDURE altaAfiliado
 	@af_rel_id TINYINT,
@@ -26,12 +26,14 @@ AS
 BEGIN
 	INSERT INTO afiliado(af_rel_id, us_id, af_nombre ,af_apellido ,af_tipodoc, af_numdoc, af_direccion , af_telefono , af_mail , af_nacimiento , af_estado_civil, af_cantidad_familiares, planmed_id , af_sexo)
 	VALUES (@af_rel_id, @us_id, @af_nombre , @af_apellido , @af_tipodoc, @af_numdoc, @af_direccion , @af_telefono , @af_mail , @af_nacimiento , @af_estado_civil, 0, @planmed_id , @af_sexo)
+END
 
 CREATE PROCEDURE getPlanesMedicos
 AS
 BEGIN
 SELECT planmed_id
 FROM Plan_medico
+END
 
 CREATE PROCEDURE getRolesPorUsuario
 	@us_id INT
@@ -41,7 +43,7 @@ BEGIN
         FROM rol r 
 	JOIN rol_por_usuarios u ON r.rol_id = u.rol_id 
 	WHERE u.id_usuario = @us_id
-                
+END
 
 CREATE PROCEDURE getFuncionalidadesPorRol
 	@rol_id INT
@@ -52,10 +54,11 @@ BEGIN
 	JOIN funcionalidad_por_rol r
 	ON f.fun_id = r.fun_id
 	WHERE r.rol_id = @rol_id
-
+END
 
 CREATE PROCEDURE getAllRoles
 
 AS
 BEGIN
 	SELECT * FROM rol
+END
