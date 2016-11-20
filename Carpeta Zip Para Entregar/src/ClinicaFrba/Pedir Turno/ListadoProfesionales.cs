6fcb78e3dedd64bfa86f2529c82036f295baccdf
datto96 <lucasdattoli96@gmail.com>
2016-11-20 13:24:42 -0300

    Seleccion de profesional es un caño

--- Carpeta Zip Para Entregar/src/ClinicaFrba/Pedir Turno/ListadoProfesionales.cs
+++ Carpeta Zip Para Entregar/src/ClinicaFrba/Pedir Turno/ListadoProfesionales.cs
@@ -17,25 +17,27 @@ namespace ClinicaFrba.Pedir_Turno
     {
         
         String wheres;
-        String nombreUsuario
+        String nombreAfiliado;
 
-        public ListadoProfesionales(String nombreUsuarioPasado)
+        public ListadoProfesionales(String nombreAfiliadoPasado)
         {
             InitializeComponent();
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView1.MultiSelect = false;
             dataGridView1.ReadOnly = true;
 
-            nombreUsuario = nombreUsuarioPasado;
+            nombreAfiliado = nombreAfiliadoPasado;
 
             string comando = "SELECT * FROM especialidad";
             DataTable dt = (new BDConnection()).cargarTablaSQL(comando);
+
+          
 
             ChkListEspecialidades.Items.Clear();
             for (int i = 0; i <= (dt.Rows.Count - 1); i++)
             {
                 int idf = Convert.ToInt32(dt.Rows[i][0]);
-                ChkListEspecialidades.Items.Insert(i, new Especialidades(idf, dt.Rows[i][2].ToString(), this));
+                ChkListEspecialidades.Items.Insert(i, new Especialidades(idf, dt.Rows[i][1].ToString(), this));
             }
 
             inicializar();
@@ -48,11 +50,15 @@ namespace ClinicaFrba.Pedir_Turno
             txtDescrip.Text = "";
 
 
+         
+
              for (int i = 0; i <= ChkListEspecialidades.Items.Count - 1; i++)
              {
-                 ChkListEspecialidades.SetItemCheckState(i, CheckState.Checked);
+                 ChkListEspecialidades.SetItemCheckState(i, CheckState.Unchecked);
              }
 
+             ChkListEspecialidades.SetItemCheckState(0, CheckState.Checked);
+             
              filtrarPag(1);
 
          }
@@ -69,7 +75,7 @@ namespace ClinicaFrba.Pedir_Turno
              
             if (txtDescrip.Text != "")
             {
-                wheres = wheres + " AND profesional.prof_apellido LIKE '%" + txtDescrip.Text + "%'";
+                wheres = wheres + " AND p.prof_apellido LIKE '%" + txtDescrip.Text + "%'";
             }
    
          }
@@ -86,7 +92,7 @@ namespace ClinicaFrba.Pedir_Turno
             wheres = "";
             armarWhere();
 
-            string query2 = "SELECT prof_id, prof_nombre, prof_apellido FROM especialidad e JOIN especialidad_por_profesional r ON (e.esp_id = r.esp_id) JOIN profesional p ON ( r.prof_id = p.prof_id) WHERE esp_descripcion IN " + wheres;
+            string query2 = "SELECT DISTINCT(p.prof_id), prof_nombre, prof_apellido FROM especialidad e JOIN especialidad_por_profesional r ON (e.esp_id = r.esp_id) JOIN profesional p ON ( r.prof_id = p.prof_id) WHERE esp_descripcion IN " + wheres;
             CompletadorDeTablas.hacerQuery(query2, ref dataGridView1);
         }
 
@@ -122,7 +128,7 @@ namespace ClinicaFrba.Pedir_Turno
                 DataGridViewRow row = this.dataGridView1.SelectedRows[0];
                 string profesional_id = row.Cells["prof_id"].Value.ToString();
                 string profesional_apellido = row.Cells["prof_apellido"].Value.ToString();
-                Pedir_Turno.ElegirTurno turno = new Pedir_Turno.ElegirTurno(profesional_id, profesional_apellido, nombreUsuario);
+                Pedir_Turno.ElegirTurno turno = new Pedir_Turno.ElegirTurno(profesional_id, profesional_apellido, nombreAfiliado);
                 turno.ShowDialog();
 
             }
@@ -162,10 +168,7 @@ namespace ClinicaFrba.Pedir_Turno
 
         private void buttonReestablecer_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i <= ChkListEspecialidades.Items.Count - 1; i++)
-            {
-                ChkListEspecialidades.SetItemCheckState(i, CheckState.Unchecked);
-            }
+
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -186,6 +189,14 @@ namespace ClinicaFrba.Pedir_Turno
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void buttonReestablecer_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= ChkListEspecialidades.Items.Count - 1; i++)
+            {
+                ChkListEspecialidades.SetItemCheckState(i, CheckState.Unchecked);
+            }
         }
     }
 }
